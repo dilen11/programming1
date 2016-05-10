0a6541e570a775312bea777399916ba1659b888e
Dilendra Khanal <dkhanald@gmail.com>
2016-05-10 18:51:08 -0500

    Create programme 4 source.cpp

---
+++ programme 4 source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+
+
+
+
+
+
+
+int main() {
+
+	ifstream input("lib.txt");
+	string word;
+
+
+
+
+
+
+	return 0;
+}
